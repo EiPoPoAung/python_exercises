957dd8af362b5400b6da5bc909967c344ae41754
laminmohmoh <39266022+laminmohmoh@users.noreply.github.com>
2018-06-07 10:41:58 +0630

    python exercise7

---
+++ ex7.py
@@ -0,0 +1,18 @@
+print("Mary had a little lamb")
+print("It's fleece was white as %s."%'snow')
+print("And everything that Mary went.")
+print("." *10 )
+endl1="C"
+endl2="h"
+endl3="e"
+endl4="e"
+endl5="s"
+endl6="e"
+endl7="B"
+endl8="u"
+endl9="r"
+endl10="g"
+endl11="e"
+endl12="r"
+print(endl1+endl2+endl3+endl4+endl5+endl6,)
+print(endl7+endl8+endl9+endl10+endl11+endl12)
