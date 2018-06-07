9fcd9d633500d15db6e927897600835cf5a2e1af
EiPoPoAung <eipopoaung.cheepo123@gmail.com>
2018-06-07 11:52:50 +0630

    add

--- ex14.py
+++ ex14.py
@@ -1,11 +1,11 @@
 from sys import argv
 script,user_name=argv
 prompt='>'
-print "Hi %s,I'm the %s script."%(user_name,script)
-print("I'd like to ask you a few question.")
-print("Do you like me %s?"%user_name)
+print (f"Hi %s,I'm the %s script."%(user_name,script)
+print(f"I'd like to ask you a few question.")
+print(f"Do you like me %s?"%user_name)
 likes=input(prompt)
-print("Where do you live %s?"%user_name)
+print(f"Where do you live %s?"%user_name)
 lives=input(prompt)
 print("Whar kind of computer do you have")
 computer=input(prompt)
