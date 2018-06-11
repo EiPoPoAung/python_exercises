d1e28473e7d635b5f9a6270ae9028e28c4076ef1
EiPoPoAung <eipopoaung.cheepo123@gmail.com>
2018-06-11 19:54:25 +0630

    finish ex30

---
+++ ex30.py
@@ -0,0 +1,19 @@
+people=30
+cats=40
+trucks=15
+if cars> people:
+    print("We should takes the cars.")
+elif cars<people:
+    print("We should not takes the cars")
+else:
+    print("We can't decide")
+if trucks>cars:
+    print("That is too many trucks")
+elif trucks<cars:
+    print("May be we could take the trucks")
+else:
+    print("We still can't decide")
+if people>trucks:
+    print("Alright,let's just take the trucks")
+else:
+    print("Fine,let's stay home then.")
