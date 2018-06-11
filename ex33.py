9f6ba5b7addb70e8b76026ecb74db3afa017fd2b
EiPoPoAung <eipopoaung.cheepo123@gmail.com>
2018-06-12 06:24:24 +0630

    finish ex33

---
+++ ex33.py
@@ -0,0 +1,12 @@
+i=0
+numbers=[]
+while i<6:
+    print(f"At the top i is {i}")
+    mumbers.append(i)
+    i=i+1
+    print("Numbers now:",numbers)
+    print(f"At the bottom i is {i}")
+print("The number:")
+for num in numbers:
+    print(num)
+    
